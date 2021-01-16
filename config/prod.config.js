97fbf1de7e08708e535a62f16566a08ed30f2264
gyanesh <gyanesh.gyanesh@gmail.com>
2021-01-16 14:17:13 +0530

    API

---
+++ config/prod.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "3.7.31.207",
+    USER: "root",
+    PASSWORD: "RBI@M0ve@1980",
+    DB: "spicescreen",
+    dialect: "mariadb",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
